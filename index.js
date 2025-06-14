d1260698a50c77a7c14a4c4042a27158fa91429c
Muneeb Atta Khan <30898560+muneebattamindravel@users.noreply.github.com>
2025-06-14 14:28:00 +0500

    quick update

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const { exec } = require('child_process');
 const { spawn, exec } = require('child_process');
 
 const app = express();
